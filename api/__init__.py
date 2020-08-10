40b8ae2da29c3e191b8e3d76d6b228f4ffdd56f5
tuckerthomas <tuckerthomas@users.noreply.github.com>
2020-08-10 15:27:46 -0400

    Forgot to remove some leftover imports from a previous concept

--- api/__init__.py
+++ api/__init__.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from flask_marshmallow import Marshmallow
 
 # Initialize the whole api
 def create_app():
@@ -14,5 +13,4 @@ def create_app():
     # Submodule Initialization
     models.init_app(app)
     routes.init_app(app)
-
     return app
