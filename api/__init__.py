d0f94d4b3f0d817cc67b3d8d04df5c5b72e7cef4
Justin Campbell <campbell.justin.tyler@gmail.com>
2020-08-11 18:55:31 -0400

    Update Docs

--- api/__init__.py
+++ api/__init__.py
@@ -2,10 +2,14 @@ from flask import Flask
 from . import models, routes
 from api.security import ta, ph
 
-# App instance creation
+# Create Flask App Instance
 app = Flask(__name__)
-# Configuration
+
+# Configure SQLAlchmey to use SQLite
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../personal_finance-dev.db'
 
+# Initialize SQLAlchemy and Marshmallow
 models.init_app(app)
+
+# Initialize Flask-RESTFul Models
 routes.init_app(app)
